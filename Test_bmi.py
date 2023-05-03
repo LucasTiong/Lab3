b39ea6c22256fdc275c94fea376a108d17940fd4
Lucas Tiong <lucastiong.22@ichat.sp.edu.sg>
2023-05-03 15:46:55 +0800

    This is to commit test_bmi file

---
+++ Test_bmi.py
@@ -0,0 +1,19 @@
+import unittest
+import lab2.bmi as bmi
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+def Test_bmi_normal_weight()
+    if bmi == -1:
+        print("Under Weight")
+    elif bmi == 0:
+        print("Normal Weight")
+    elif bmi == 1:
+        print("Over Weight")
