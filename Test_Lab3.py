935f8b8073a3ceff8e1234b534759a8282b42730
Lucas Tiong <lucastiong.22@ichat.sp.edu.sg>
2023-05-04 12:15:14 +0800

    This is to commit changes

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -28,3 +28,15 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_sort_too_many_numbers():
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 1, 2, 3, 4]
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 1)
+
+def test_bubble_sort_no_numbers():
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 0)
--- Test_bmi.py
+++ Test_bmi.py
@@ -1,19 +1,14 @@
-import unittest
 import lab2.bmi as bmi
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
-
-
-def Test_bmi_normal_weight()
-    if bmi == -1:
-        print("Under Weight")
-    elif bmi == 0:
-        print("Normal Weight")
-    elif bmi == 1:
-        print("Over Weight")
+def test_bmi_over_weight():
+    weight, height = 100, 1.40
+    result = bmi.calculate_bmi(weight, height)
+    assert(result == -1)
+def test_bmi_normal_weight():
+    weight, height = 57, 1.73
+    result = bmi.calculate_bmi(weight, height)
+    assert(result == -1)
+def test_bmi_under_weight():
+    weight, height = 50, 1.80
+    result = bmi.calculate_bmi(weight, height)
+    assert(result == -1)
